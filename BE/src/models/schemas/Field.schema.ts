010471bd7d71298214ee1745a9624e240828d480
Walt25 <trantienn25@gmail.com>
2024-04-24 11:01:16 +0700

    add field name attr

--- BE/src/models/schemas/Field.schema.ts
+++ BE/src/models/schemas/Field.schema.ts
@@ -2,6 +2,7 @@ import { ObjectId } from 'mongodb'
 
 interface FieldType {
   _id?: ObjectId
+  name: string
   images: string[]
   location: string
   promotion_id?: ObjectId | undefined
@@ -9,11 +10,13 @@ interface FieldType {
 
 export default class Field {
   _id?: ObjectId
+  name: string
   images: string[]
   location: string
   promotion_id?: ObjectId
   constructor(field: FieldType) {
     this._id = field._id
+    this.name = field.name
     this.images = field.images
     this.location = field.location
     this.promotion_id = field.promotion_id || undefined
--- BE/src/services/field.services.ts
+++ BE/src/services/field.services.ts
@@ -4,7 +4,9 @@ import { ObjectId } from 'mongodb'
 
 class FieldService {
   async addNewField(body: any) {
-    const result = await databaseService.fields.insertOne(new Field({ images: body.images, location: body.location }))
+    const result = await databaseService.fields.insertOne(
+      new Field({ images: body.images, location: body.location, name: body.name })
+    )
     const field = await databaseService.fields.findOne({ _id: new ObjectId(result.insertedId) })
     return field
   }
